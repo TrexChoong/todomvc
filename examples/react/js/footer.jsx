7917a0d9e9dad979fe541ba17711a55b4c845f8a
TrexChoong <guanstrife@gmail.com>
2022-06-14 15:49:55 +0800

    Update footer.jsx
    Update footer to show full list count

--- examples/react/js/footer.jsx
+++ examples/react/js/footer.jsx
@@ -27,7 +27,7 @@ var app = app || {};
 			return (
 				<footer className="footer">
 					<span className="todo-count">
-						<strong>{this.props.count}</strong> {activeTodoWord} left
+						<strong>{this.props.count} / {this.props.count + this.props.completedCount}</strong> {activeTodoWord} left
 					</span>
 					<ul className="filters">
 						<li>
